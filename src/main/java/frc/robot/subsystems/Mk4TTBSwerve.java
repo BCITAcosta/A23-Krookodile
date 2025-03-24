850474ba5a579ca6ec195614e60b8d95e75ecafb
BCITAcosta <119699836+BCITAcosta@users.noreply.github.com>
2025-03-24 15:05:25 -0400

    Inverted Drive Encoder

--- src/main/java/frc/robot/subsystems/Mk4TTBSwerve.java
+++ src/main/java/frc/robot/subsystems/Mk4TTBSwerve.java
@@ -84,6 +84,7 @@ public class Mk4TTBSwerve{
         m_driveSparkMaxConfig.idleMode(IdleMode.kBrake);
         m_driveSparkMaxConfig.inverted(m_constants.driveInverted);
         m_driveSparkMaxConfig.smartCurrentLimit(50);
+        m_driveSparkMaxConfig.encoder.inverted(true);
         m_driveSparkMaxConfig.encoder.positionConversionFactor(SwerveDriveConstants.kDrivingEncoderPositionFactor);
         m_driveSparkMaxConfig.encoder.velocityConversionFactor(SwerveDriveConstants.kDrivingEncoderVelocityFactor);
         m_driveSparkMaxConfig.closedLoop
